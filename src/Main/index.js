bab15b4ffc4ce199fa7eed5353e0d904bc01c737
Vijay Tripathi <vijayxtri@gmail.com>
2019-12-18 22:30:15 +0530

    Completed

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
-import { Container, Card, Row, Col, ListGroup, Badge } from "react-bootstrap";
+import { Switch, Route } from "react-router-dom";
+import { Jumbotron } from "react-bootstrap";
 
 import axios from "axios";
 
 import Mission from "../Mission";
 import { Loader } from "../Extra/Spinner";
+import LaunchpadPage from "../Launchpad";
+import PayloadClass from "../Payload";
 
 class Space extends React.Component {
   constructor(props) {
@@ -28,7 +30,6 @@ class Space extends React.Component {
     axios
       .get("https://api.spacexdata.com/v3/launchpads")
       .then(res => {
-        console.log(res);
         this.setState({ launchData: res.data });
       })
       .catch(function(error) {
@@ -49,83 +50,42 @@ class Space extends React.Component {
         ) : (
           <Loader></Loader>
         )}
-        <Route path="/launchpad">
-          {this.state.launchData ? (
-            <Container style={{ marginTop: "2%" }}>
-              <Row>
-                {this.state.launchData.map((item, index) => {
-                  return (
-                    <Col xs={12} md={4} s={6} l={4} key={index}>
-                      <Card key={item.id}>
-                        <Card.Header>
-                          <Badge
-                            pill
-                            variant={
-                              item.status === "active"
-                                ? "success"
-                                : item.status === "retired"
-                                ? "info"
-                                : item.status === "under construction"
-                                ? "warning"
-                                : "danger"
-                            }
-                            style={{ float: "left", color: "transparent" }}
-                          >
-                            .
-                          </Badge>
-                          {item.site_id}
-                        </Card.Header>
-                        <Card.Body id="style-1">
-                          <blockquote className="blockquote mb-0">
-                            {/* <p style={{ fontSize: "0.9rem" }}> */}
-                            <ListGroup variant="flush">
-                              <ListGroup.Item>
-                                <span style={{ fontWeight: "bold" }}>
-                                  Location:
-                                </span>{" "}
-                                {item.location.name}
-                              </ListGroup.Item>
-                              <ListGroup.Item>
-                                <span style={{ fontWeight: "bold" }}>
-                                  Regin:
-                                </span>{" "}
-                                {item.location.region}
-                              </ListGroup.Item>
-                              <ListGroup.Item>
-                                <span style={{ fontWeight: "bold" }}>
-                                  Coordinates:
-                                </span>{" "}
-                                {item.location.latitude}/
-                                {item.location.longitude}
-                              </ListGroup.Item>
-                              {/* <ListGroup.Item></ListGroup.Item> */}
-                            </ListGroup>
-                            <p style={{ fontSize: "0.9rem", marginTop: "10%" }}>
-                              {" "}
-                              {item.details}{" "}
-                            </p>
-                            {/* </p> */}
+        {this.state.launchData ? (
+          <Route
+            path="/launchpad"
+            render={props => (
+              <LaunchpadPage
+                {...props}
+                launchData={this.state.launchData}
+              ></LaunchpadPage>
+            )}
+          ></Route>
+        ) : (
+          <Loader></Loader>
+        )}
+        <Route
+          path="/payload/:id?"
+          render={props => <PayloadClass {...props}></PayloadClass>}
+        ></Route>
 
-                            {/* {item.payload_ids.map((element, index) => (
-                              <footer className="blockquote-footer" key={index}>
-                                <cite title="Source Title">
-                                  <Link to={`/payload/${element}`}>
-                                    {element}
-                                  </Link>
-                                </cite>
-                              </footer>
-                            ))} */}
-                          </blockquote>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  );
-                })}
-              </Row>
-            </Container>
-          ) : null}
+        <Route path="/">
+          <Jumbotron className="centerDivJumbo">
+            <img
+              src="https://www.spacex.com/sites/spacex/files/spacex_logo_white.png"
+              alt="Home"
+              width="30%"
+            ></img>
+            <p
+              style={{
+                marginTop: "2%",
+                fontFamily: "'Alatsi', sans-serif",
+                marginLeft: "-3%"
+              }}
+            >
+              Application that provides information about SpaceX resources
+            </p>
+          </Jumbotron>
         </Route>
-        <Route path="/"></Route>
       </Switch>
     );
   }
--- src/Mission/index.js
+++ src/Mission/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import { Container, Card, Row, Col } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+
+import CardComponent from "../Card";
 
 const Mission = props => {
   return (
@@ -9,23 +11,20 @@ const Mission = props => {
         {props.data.map((item, index) => {
           return (
             <Col xs={12} md={6} sm={6} lg={4} key={index}>
-              <Card key={item.id}>
-                <Card.Header>{item.mission_name}</Card.Header>
-                <Card.Body id="style-1">
-                  <blockquote className="blockquote mb-0">
-                    <p style={{ fontSize: "0.9rem" }}> {item.description} </p>
-                    <strong>Pay Load Id</strong>
+              <CardComponent id={item.id} name={item.mission_name}>
+                <blockquote className="blockquote mb-0">
+                  <p style={{ fontSize: "0.9rem" }}> {item.description} </p>
+                  <strong>Pay Load Id</strong>
 
-                    {item.payload_ids.map((element, index) => (
-                      <footer className="blockquote-footer" key={index}>
-                        <cite title="Source Title">
-                          <Link to={`/payload/${element}`}>{element}</Link>
-                        </cite>
-                      </footer>
-                    ))}
-                  </blockquote>
-                </Card.Body>
-              </Card>
+                  {item.payload_ids.map((element, index) => (
+                    <footer className="blockquote-footer" key={index}>
+                      <cite title="Source Title">
+                        <Link to={`/payload/${element}`}>{element}</Link>
+                      </cite>
+                    </footer>
+                  ))}
+                </blockquote>
+              </CardComponent>
             </Col>
           );
         })}
