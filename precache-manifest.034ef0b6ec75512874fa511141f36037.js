29c7bf0220b65675a197fe2b340289342c57d53c
Vijay Tripathi <vijayxtri@gmail.com>
2019-12-18 22:40:06 +0530

    Updates

---
+++ precache-manifest.034ef0b6ec75512874fa511141f36037.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a4dbdc6039e949ff430e8533f93c27",
+    "url": "/SpaceX/index.html"
+  },
+  {
+    "revision": "f6685df82f6166c8d934",
+    "url": "/SpaceX/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "14a67798531e1e8d2f24",
+    "url": "/SpaceX/static/css/main.7457d8a9.chunk.css"
+  },
+  {
+    "revision": "f6685df82f6166c8d934",
+    "url": "/SpaceX/static/js/2.b84fc8db.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/SpaceX/static/js/2.b84fc8db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14a67798531e1e8d2f24",
+    "url": "/SpaceX/static/js/main.90021ea3.chunk.js"
+  },
+  {
+    "revision": "8db7f1c874abaa969f36",
+    "url": "/SpaceX/static/js/runtime-main.d00a7da9.js"
+  }
+]);
